2ed6fa18b5b246e2fd40de97dc469cf7faba750e
Javier Aguilar <coyote176@msn.com>
2020-08-01 02:30:54 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,12 @@
-def unsafe?(speed)
-if speed < 40 || speed > 60
+def unsaf?(spee)
+if speed<40||speed > 60
   return true
 else
   return false
  end
 end
 def not_safe?(speed)
-#  speed > 60? "true" speed < 40 : "false"
- speed > 60 || speed < 40? true: false
+speed > 60 || speed < 40? true: false
  end
 
 
